88ec2bfdcc4601db04a5adaf4ac18cdf3afbe61d
zwj85123@163.com <zwj85123@163.com>
2020-09-17 15:49:35 +0800

    添加项目文件。

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Class1
+    {
+        public string LaoHu { get; set; }
+
+        public string MaoLv { get; set; }
+
+        public string LiYu { get; set; }
+
+        public string ShanYang { get; set; }
+    }
+}
---
+++ ConsoleApp1/DongWu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public interface   DongWu
+    {
+        public void DW()
+        {
+            Console.WriteLine("66");
+        }
+
+        public void Chi()
+        {
+            Console.WriteLine("66");
+        }
+        public void Shui()
+        {
+            Console.WriteLine("66");
+        }
+    }
+
+    
+}
